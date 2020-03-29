b9d07ee62b1f3b3fb11178d9f17dad33daa5b46f
huanghonghao <1250659520@qq.com>
2020-03-29 23:44:20 +0800

    添加tvb定时抓取任务

--- movie-spider/src/main/java/com/hhh/moviespider/processor/TVB56dayProcessor.java
+++ movie-spider/src/main/java/com/hhh/moviespider/processor/TVB56dayProcessor.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @Component
 public class TVB56dayProcessor implements PageProcessor {
 
-    private static final String LIST_URL = "http://www.56dy.com/ju/list/----10-.*";
+    private static final String LIST_URL = "http://www.56dy.com/ju/list/----10-";
 
     private static final String PLAY_URL = "http://www.56dy.com/ju/\\d+/play.*";
 
@@ -42,7 +42,7 @@ public class TVB56dayProcessor implements PageProcessor {
             String playPage = page.getHtml().xpath("//div[@class='stui-player']/div[@class='stui-player__video']/iframe/@src").get();
             if(StringUtils.isNotBlank(playPage)) {
                 String url = page.getHtml().xpath("//h1[@class='title']/a/@href").get();
-                String name = page.getHtml().xpath("//h1[@class='title']/a/text()").regex("(?<=第)\\d+(?=集)").get();
+                String name = page.getHtml().xpath("//h1[@class='title']/a/text()").regex("(?<=第).*(?=集)").get();
                 page.addTargetRequest(playPage + "?movieId=" + url.substring(url.lastIndexOf("/") +1) + "&name=" + name);
             }
         } else {
--- movie-spider/src/main/java/com/hhh/moviespider/task/MovieTask.java
+++ movie-spider/src/main/java/com/hhh/moviespider/task/MovieTask.java
@@ -1,11 +1,16 @@
 package com.hhh.moviespider.task;
 
 import com.hhh.moviespider.model.BDMovie;
+import com.hhh.moviespider.model.TVB56dy;
 import com.hhh.moviespider.pipeline.BDMoviePipeline;
+import com.hhh.moviespider.pipeline.TVB56dyEpisodesPipeline;
+import com.hhh.moviespider.pipeline.TVB56dyPipeline;
+import com.hhh.moviespider.processor.TVB56dayProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.downloader.HttpClientDownloader;
 import us.codecraft.webmagic.model.OOSpider;
 import us.codecraft.webmagic.proxy.Proxy;
@@ -19,6 +24,15 @@ public class MovieTask {
     @Autowired
     private BDMoviePipeline bdMoviePipeline;
 
+    @Autowired
+    private TVB56dyPipeline tvb56dyPipeline;
+
+    @Autowired
+    private TVB56dayProcessor tvb56dayProcessor;
+
+    @Autowired
+    private TVB56dyEpisodesPipeline tvb56DyEpisodesPipeline;
+
     @Scheduled(cron = "0 0 0 * * *")
     public void bdMovieSpider() {
 //        HttpClientDownloader httpClientDownloader = new HttpClientDownloader();
@@ -31,5 +45,24 @@ public class MovieTask {
                 .addUrl("https://www.bd-film.cc/movies/index.htm")
                 .thread(5)
                 .run();
+    }
+
+    @Scheduled(cron = "0 16 21,23 * * *")
+    public void tvb56dySpider() {
+        OOSpider.create(Site.me().setSleepTime(1000).setTimeOut(10000).setCycleRetryTimes(5)
+                , tvb56dyPipeline, TVB56dy.class)
+                .setScheduler(new QueueScheduler().setDuplicateRemover(new BloomFilterDuplicateRemover(30000)))
+                .addUrl("http://www.56dy.com/ju/list/----10-")
+                .thread(6)
+                .run();
+    }
+
+    @Scheduled(cron = "0 15 21,23 * * *")
+    public void tvb56dyEPSpider() {
+        Spider.create(tvb56dayProcessor)
+                .addPipeline(tvb56DyEpisodesPipeline)
+                .setScheduler(new QueueScheduler().setDuplicateRemover(new BloomFilterDuplicateRemover(30000)))
+                .addUrl("http://www.56dy.com/ju/list/----10-")
+                .thread(30).run();
     }
 }
